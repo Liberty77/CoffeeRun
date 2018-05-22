864ce3625c62997b5887ddb78c540800ad5dde07
Liberty77 <1037091775@qq.com>
2018-05-22 22:09:08 +0800

    complate

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,14 +1,31 @@
-
 (function(window) {
     'use strict';
 
-    var App = window.App || {};  //声明的一个本地变量
+    var App = window.App || {}; //声明的一个本地变量
 
     function DataStore() {
 
+        this.data = {};
+
     }
 
-    App.DataStore = DataStore;    //赋值给全局变量
+    DataStore.prototype.add = function(key, val) {
+        this.data[key] = val;
+    }
+
+    DataStore.prototype.get = function(key) {
+        return this.data[key];
+    }
+
+    DataStore.prototype.getAll = function() {
+        return this.data;
+    }
+
+    DataStore.prototype.remove = function() {
+        delete this.data[key];
+    }
+
+    App.DataStore = DataStore; //赋值给全局变量
     window.App = App;
 
 })(window);
