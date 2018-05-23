3d64108142f1ef0059ebcd00b63b9dcfd8ad7474
高八度 <1037091775@qq.com>
2018-05-23 15:16:55 +0800

    180523

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,12 +1,14 @@
 
 (function(window) {
     'use strict';
-
     var App = window.App || {};  //声明的一个本地变量
 
     function DataStore() {
 
+        this.data = {};
+
     }
+
 
     App.DataStore = DataStore;    //赋值给全局变量
     window.App = App;
